1e6e29ef732491e9d60169741d9f5ed0adee98ae
Sathish <150118229+satishmscict@users.noreply.github.com>
2025-05-17 12:16:10 +0530

    Code tidy

--- src/main/java/com/assignment/college_management_system/controllers/StudentController.java
+++ src/main/java/com/assignment/college_management_system/controllers/StudentController.java
@@ -3,7 +3,6 @@ package com.assignment.college_management_system.controllers;
 import com.assignment.college_management_system.dtos.StudentDTO;
 import com.assignment.college_management_system.services.StudentService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
